96a98580b26b0d583a98408e77ee510c234b0d51
Remy <remy@emesa.nl>
2019-03-12 15:43:39 +0100

    Attempt at having offset - X functionality.

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -7,32 +7,44 @@ TIMEOUT = 10.0
 class KafkaConsumer():
     config = {}
     limit = 100
+    offsets = {}
 
     def __init__(self, kafka_settings, limit=100):
         config = {
             'error_cb': self.error_callback,
-            **kafka_settings
+            **kafka_settings,
+            'auto.offset.reset': 'earliest',
         }
         self.config = config
         self.limit = int(limit)
 
     def topic_assigned(self, consumer, partitions):
         for p in partitions:
-            p.offset = OFFSET_BEGINNING
+            offset = self.offsets.get((p.topic, p.partition)) - self.limit - 1
+            if offset < 0:
+                offset = OFFSET_BEGINNING
+            p.offset = offset
         consumer.assign(partitions)
 
     def consume(self, topic):
         consumer = Consumer(self.config)
         # Subscribe to topics
+        consumer.subscribe(topic)
+        self._get_messages(consumer)
         consumer.subscribe(topic, on_assign=self.topic_assigned)
+        return self._get_messages(consumer)
 
+    def _get_messages(self, consumer):
+        """ Get all the messages from the current topics."""
         count = 0
-        result = []
+        messages = []
         eof_reached = {}
         while count < self.limit:
             msg = consumer.poll(timeout=TIMEOUT)
             if msg is None:
                 continue
+
+            self.offsets[(msg.topic(), msg.partition())] = msg.offset()
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
                     eof_reached[(msg.topic(), msg.partition())] = True
@@ -42,7 +54,7 @@ class KafkaConsumer():
                 else:
                     raise Exception(msg.error())
             else:
-                result.append(
+                messages.append(
                     {
                         'key': msg.key().decode('utf8'),
                         'value': self._auto_decode(msg.value()),
@@ -50,8 +62,7 @@ class KafkaConsumer():
                     }
                 )
             count += 1
-
-        return result
+        return messages
 
     def _auto_decode(self, value):
         return avro_serializer.deserialize_first(value)
