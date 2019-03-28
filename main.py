6062f19e9c0326a42f08573a2b04de1a38523e6c
Remy <remy@emesa.nl>
2019-03-28 13:55:40 +0100

    Added export all button.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 if not os.path.isfile('config.ini'):
     print("ERROR: Remember to copy config.sample.ini to your own config.ini file!")
     import sys
+
     sys.exit(1)
 
 config = configparser.ConfigParser()
@@ -40,19 +41,23 @@ class ClientFrame(wx.Frame):
         sizer_top.Add(self.topic, 1, wx.EXPAND)
 
         # Add update button.
-        self.update_button = wx.Button(self, -1, "Update", size=(100,-1))
+        self.update_button = wx.Button(self, -1, "Update", size=(100, -1))
         self.Bind(wx.EVT_BUTTON, self.refresh_topic, self.update_button)
         sizer_top.Add(self.update_button, 0, wx.EXPAND)
+
+        self.export_button = wx.Button(self, -1, "Export ALL", size=(100, -1))
+        self.Bind(wx.EVT_BUTTON, self.export_data, self.export_button)
+        sizer_top.Add(self.export_button, 0, wx.EXPAND)
 
         sizer_bottom = wx.BoxSizer(wx.HORIZONTAL)
 
         # Message list.
-        self.list = wx.ListBox(self, size=(200,-1), style=wx.LB_SINGLE)
+        self.list = wx.ListBox(self, size=(200, -1), style=wx.LB_SINGLE)
         self.Bind(wx.EVT_LISTBOX, self.show_item, self.list)
         sizer_bottom.Add(self.list, 0, wx.EXPAND)
 
         # Json output.
-        self.output = wx.TextCtrl(self, size=(500,450), style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.output = wx.TextCtrl(self, size=(500, 450), style=wx.TE_MULTILINE | wx.TE_READONLY)
         sizer_bottom.Add(self.output, 1, wx.EXPAND)
         self.sizer_bottom = sizer_bottom
 
@@ -79,16 +84,57 @@ class ClientFrame(wx.Frame):
         self._update_messages()
 
     def show_item(self, event):
+        """
+        Show single item in main (big) pane.
+        :param event:
+        :return:
+        """
         index = self.list.GetSelection()
         if index is None or not self.messages:
             self.output.SetValue('')
         else:
             real_index = len(self.messages) - 1 - index
             item = self.messages[real_index]
-            self.output.SetValue(json.dumps(item['value'], sort_keys=False, indent=4))
+            self.output.SetValue(self._to_json_string(item['value']))
 
     def show_error(self, string):
+        """
+        Show error with popup.
+        :param string:
+        :return:
+        """
         wx.MessageBox(string, style=wx.ICON_ERROR | wx.CENTRE)
+
+    def export_data(self, event):
+        """ Export all data. """
+
+        with wx.FileDialog(self, "Save file", defaultFile="%s.json" % self.topic.GetValue(),
+                           style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT) as fileDialog:
+
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return  # Don't continue, user cancelled.
+
+            # Save to file.
+            pathname = fileDialog.GetPath()
+            try:
+                with open(pathname, 'w') as file:
+                    file.write(self._get_export_data())
+            except IOError:
+                self.show_error("Unable to save in: %s" % pathname)
+
+    def _get_export_data(self):
+        """
+        Get export data of all current messages in json string.
+        :return:
+        """
+        json_dict = {}
+        for message in self.messages:
+            json_dict["%s - %s" % (message['offset'], message['key'])] = message['value']
+
+        return self._to_json_string(json_dict)
+
+    def _to_json_string(self, data):
+        return json.dumps(data, sort_keys=False, indent=4)
 
     def _update_messages(self):
         topic_name = self.topic.GetValue()
@@ -119,5 +165,5 @@ class ClientFrame(wx.Frame):
 
 
 app = wx.App()
-frm = ClientFrame(None, title='Kafka Consumer', size=(500,500))
+frm = ClientFrame(None, title='Kafka Consumer', size=(500, 500))
 app.MainLoop()
